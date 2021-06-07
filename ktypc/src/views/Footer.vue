29de4f125846577351fb30c55c0199abe4ad4f42
qite666 <15243986018@139.com>
2021-06-07 22:20:39 +0800

    20210607

--- ktypc/src/views/Footer.vue
+++ ktypc/src/views/Footer.vue
@@ -1,6 +1,121 @@
 <template>
-    <footer>我是页脚</footer>
+  <div class="body-fot">
+    <div class="fot">
+      <div class="fot-top">
+        <div class="left-center fl">
+          <h2>锦缘国际夜总会</h2>
+          <span>
+            成都夜总会，成都夜场，成都酒吧各种模特佳丽【13688143752】，设备齐全，装修高端，资源丰富，生意每天开到爆，欢迎随时预定包厢
+          </span>
+          <span>咨询电话：13688143752</span>
+          <span>公司地址：高端夜场夜总会</span>
+        </div>
+        <div class="left-code fr">
+          <img src="../assets/img/patrick.png" alt="" />
+          <span>关注加好友更优惠</span>
+        </div>
+      </div>
+      <div class="fot-bot">
+        <p>
+          Copyright @ 2021. All rights reserved.高端夜场夜总会 版权所有.
+          京ICP备11058096号
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
-<style>
+<style lang="less">
+@import "../assets/css/base.css";
+.body-fot {
+  width: 100%;
+  height: 328px;
+  background-color: rgb(38, 44, 56);
+  .fot {
+    width: 1200px;
+    height: 328px;
+    margin: 0 auto;
+    .fot-top {
+      width: 100%;
+      height: 274px;
+      border-bottom: 1px solid rgb(152, 152, 152);
+      .left-center {
+        text-align: left;
+        h2 {
+          font-weight: 400;
+          font-size: 36px;
+          user-select: text;
+          margin: 40px 0 30px;
+          color: rgb(255, 255, 255);
+        }
+        span {
+          display: block;
+          user-select: text;
+        }
+        span:nth-of-type(1) {
+          font-size: 14px;
+          color: #989898;
+          width: 496px;
+          cursor: default;
+          margin-bottom: 50px;
+          animation: move 1s;
+        }
+        span:nth-of-type(2) {
+          font-size: 14px;
+          color: #00b7d9;
+          cursor: text;
+          margin-bottom: 8px;
+          animation: move 1s;
+        }
+        span:nth-of-type(3) {
+          font-size: 14px;
+          cursor: text;
+          color: #00b7d9;
+          animation: move 1s;
+        }
+      }
+      .left-code {
+        width: 162px;
+        height: 100%;
+        overflow: hidden;
+        img {
+          width: 162px;
+          height: 162px;
+          cursor: pointer;
+          margin: 40px 0 5px;
+          animation: move 1s;
+        }
+        span {
+          display: block;
+          font-size: 14px;
+          cursor: text;
+          user-select: text;
+          color: #989898;
+        }
+      }
+    }
+    .fot-bot {
+      width: 100%;
+      height: 53px;
+      line-height: 53px;
+      text-align: center;
+      p {
+        font-size: 12px;
+        cursor: text;
+        user-select: text;
+        color: rgb(152, 152, 152);
+      }
+    }
+  }
+}
+@keyframes move {
+  0% {
+    opacity: 0;
+    transform: translate(0, 274px);
+  }
 
+  100% {
+    opacity: 1;
+    transform: translate(0, 0);
+  }
+}
 </style>
--- ktypc/src/views/Header.vue
+++ ktypc/src/views/Header.vue
@@ -1,6 +1,117 @@
 <template>
-    <header>我是头部</header>
+  <div class="top">
+    <div class="logo">
+      <img src="../assets/img/logo.png" alt="" />
+    </div>
+    <div class="tab">
+      <ul>
+        <li>
+          <a href="javascript:;">首页</a>
+        </li>
+        <li>
+          <a href="javascript:;">关于我们</a>
+        </li>
+        <li>
+          <a href="javascript:;">模特展示</a>
+        </li>
+        <li>
+          <a href="javascript:;">环境展示</a>
+        </li>
+        <li>
+          <a href="javascript:;">夜场新闻</a>
+        </li>
+        <li>
+          <a href="javascript:;">联系我们</a>
+        </li>
+        <li>
+          <a href="javascript:;">招聘要求</a>
+        </li>
+      </ul>
+    </div>
+    <div class="msg">
+      <span>13688143752</span>
+    </div>
+    <div class="img">
+      <img src="../assets/img/phone.png" alt="" />
+    </div>
+  </div>
 </template>
-<style>
-
+<style lang="less">
+@import "../assets/css/base.css";
+.top {
+  display: flex;
+  width: 1200px;
+  height: 101px;
+  margin: 0 auto;
+  position: relative;
+  .logo {
+    width: 249px;
+    height: 100%;
+    line-height: 101px;
+    img {
+      width: 100%;
+      height: 31px;
+      cursor: pointer;
+    }
+  }
+  .tab {
+    width: 700px;
+    height: 101px;
+    line-height: 101px;
+    ul {
+      display: flex;
+      li {
+        width: 100px;
+        height: 100%;
+        cursor: pointer;
+        &:nth-child(1) {
+          width: 100px;
+          height: 100%;
+          cursor: pointer;
+          a {
+            padding: 0 22px 30.5px;
+          }
+        }
+        &:hover a {
+          color: rgb(60, 181, 213);
+          border-bottom: 2px solid rgb(60, 181, 213);
+        }
+        a {
+          font-size: 14px;
+          padding: 0 22px 30.5px;
+        }
+      }
+    }
+  }
+  .msg {
+    display: flex;
+    width: 22px;
+    height: 22px;
+    line-height: 101px;
+    margin-left: 145px;
+    img {
+      width: 22px;
+      height: 22px;
+    }
+    span {
+      font-size: 16px;
+      color: #00b7d9;
+    }
+  }
+  .img {
+    position: absolute;
+    top: 40px;
+    left: 1066px;
+    width: 22px;
+    height: 22px;
+    position: absolute;
+    img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
 </style>
